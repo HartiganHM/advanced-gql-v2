d6f87893a6f18ce996b7add568b4cbabebe99448
Hugh Hartigan <hhartigan@twilio.com>
2021-06-25 10:58:26 -0600

    Add subscriptions to server

--- src/index.js
+++ src/index.js
@@ -1,19 +1,38 @@
-const {ApolloServer} = require('apollo-server')
-const typeDefs = require('./typedefs')
-const resolvers = require('./resolvers')
-const {createToken, getUserFromToken} = require('./auth')
-const db = require('./db')
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./typedefs");
+const resolvers = require("./resolvers");
+const { createToken, getUserFromToken } = require("./auth");
+const db = require("./db");
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context({req}) {
-    const token = req.headers.authorization
-    const user = getUserFromToken(token)
-    return {...db, user, createToken}
-  }
-})
+  context({ req, connection }) {
+    const context = { ...db };
+    if (connection) {
+      return { ...context, ...connection.context };
+    }
 
-server.listen(4000).then(({url}) => {
-  console.log(`🚀 Server ready at ${url}`)
-})
+    const token = req.headers.authorization;
+    const user = getUserFromToken(token);
+    return { ...db, user, createToken };
+  },
+  subscriptions: {
+    // params === req.headers
+    onConnect(params) {
+      const token = params.authToken;
+      const user = getUserFromToken(token);
+      console.log(params);
+      // Can be used to globally shut down subscription
+      if (!user) {
+        throw new Error("Nope!");
+      }
+
+      return { user };
+    },
+  },
+});
+
+server.listen(4000).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
