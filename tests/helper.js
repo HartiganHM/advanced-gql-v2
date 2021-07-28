b409192e98b48f6e815044e4f853b5783eba0875
Hugh Hartigan <hhartigan@twilio.com>
2021-07-28 09:31:38 -0600

    Add notes to test files

--- tests/helper.js
+++ tests/helper.js
@@ -7,11 +7,15 @@ const createTestServer = ctx => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    //* Don't mock all resolvers, only the ones I don't pass above
+    //* More control of test cases
     mockEntireSchema: false,
+    //* Ignore resolvers and mock out with random data that fits schema
     mocks: true,
     context: () => ctx
   })
 
+  //* Returns an object with query/mutation method
   return createTestClient(server)
 }
 
--- tests/query.test.js
+++ tests/query.test.js
@@ -18,10 +18,14 @@ describe('queries', () => {
       user: {id: 1},
       models: {
         Post: {
+          //* Mockking resolver to return specific response
           findMany: jest.fn(() => [{id: 1, message: 'hello', createdAt: 12345839, likes: 20, views: 300}])
         }
       }
     })
+
+    //* Comparing snapshots to make sure structure is the same every time
+    //* Can check props if needed, but snapshot checks everything
 
     const res = await query({query: FEED})
     expect(res).toMatchSnapshot()
