cdc9e753b9daf3ca32e6d809d74c7570e27f52e7
Hugh Hartigan <hhartigan@twilio.com>
2021-07-28 13:18:54 -0600

    Write test for me query

--- tests/query.test.js
+++ tests/query.test.js
@@ -1,5 +1,5 @@
-const gql = require('graphql-tag')
-const createTestServer = require('./helper')
+const gql = require("graphql-tag");
+const createTestServer = require("./helper");
 const FEED = gql`
   {
     feed {
@@ -10,24 +10,56 @@ const FEED = gql`
       views
     }
   }
-`
+`;
 
-describe('queries', () => {
-  test('feed', async () => {
-    const {query} = createTestServer({
-      user: {id: 1},
+const ME = gql`
+  {
+    me {
+      id
+      email
+      role
+    }
+  }
+`;
+
+describe("queries", () => {
+  test("me", async () => {
+    const { query } = createTestServer({
+      user: { id: 1 },
+      user: {
+        id: 1,
+        email: "torbear@roo.com",
+        role: "ADMIN",
+      },
+    });
+
+    const response = await query({ query: ME });
+    expect(response).toMatchSnapshot();
+  });
+
+  test("feed", async () => {
+    const { query } = createTestServer({
+      user: { id: 1 },
       models: {
         Post: {
           //* Mockking resolver to return specific response
-          findMany: jest.fn(() => [{id: 1, message: 'hello', createdAt: 12345839, likes: 20, views: 300}])
-        }
-      }
-    })
+          findMany: jest.fn(() => [
+            {
+              id: 1,
+              message: "hello",
+              createdAt: 12345839,
+              likes: 20,
+              views: 300,
+            },
+          ]),
+        },
+      },
+    });
 
     //* Comparing snapshots to make sure structure is the same every time
     //* Can check props if needed, but snapshot checks everything
 
-    const res = await query({query: FEED})
-    expect(res).toMatchSnapshot()
-  })
-})
+    const res = await query({ query: FEED });
+    expect(res).toMatchSnapshot();
+  });
+});
