4e60979c158954e0f577fd29ba85a17767bdd191
Hugh Hartigan <hhartigan@twilio.com>
2021-08-13 13:56:59 -0600

    Write FormatDateDirective

--- src/directives.js
+++ src/directives.js
@@ -1,3 +1,31 @@
-const { SchemaDirectiveVisitor } = require('apollo-server')
-const { defaultFieldResolver, GraphQLString } = require('graphql')
-const {formatDate} = require('./utils')
+const { SchemaDirectiveVisitor } = require("apollo-server");
+const { defaultFieldResolver, GraphQLString } = require("graphql");
+const { formatDate } = require("./utils");
+
+class FormatDateDirective extends SchemaDirectiveVisitor {
+  visitFieldDefinition(field) {
+    const resolver = field.resolve || defaultFieldResolver;
+    const { format: defaultFormat } = this.args;
+
+    field.args.push({
+      name: "format",
+      type: GraphQLString,
+    });
+
+    field.resolve = async (root, { format, ...rest }, ctx, info) => {
+      // When arguments from the user ARE being passed
+      const result = await resolver.call(this, root, rest, ctx, info);
+
+      // When arguments from the user are NOT being passed
+      // const result = await resolver.apply(this, args);
+
+      return formatDate(result, format || defaultFormat);
+    };
+
+    field.type = GraphQLString;
+  }
+}
+
+module.exports = {
+  FormatDateDirective,
+};
